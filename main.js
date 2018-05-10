05f79e61365a246bdacba5886fcb12b586f1cdb6
Caitlin Patton <kittyluva2@gmail.com>
2018-05-10 13:51:03 -0700

    Lots of dun goofed. Added Destiny Message

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-import * as Discord from 'discord.js';
-
-const config = require('./config.json');
-const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+const { prefix, token, channel_greeting } = require('./config.json');
+const client = new Client();
 
 client.on('ready', () => {
     console.log('Endbot ready for chat!');
 });
 
 client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', config.channel-greeting);
+    const channel = member.guild.channels.find('name', channel-greeting);
     if (!channel) return;
     channel.send(`Welcome to the Endless, ${member}! What's your PSN? What game are you here for?`);
 });
 
-// This is just a silly command that lets me know Endbot is active. If "ping" doesn't work, then EndBot has probably crashed.
 client.on('message', message => {
     if (message.content === `${prefix}ping`) {
       message.reply('pong');
@@ -32,21 +30,37 @@ client.on('message', message => {
     }
 });
 
-// Destiny 2 Prompt for Webb, since he uses it all the time.
 client.on('message', message => {
     if (message.content === `${prefix}destiny`) {
-        const destinyEmbed = new Discord.MessageEmbed()
-
-        .setTitle("The Endless")
-        .setColor(0x0099ff)
-        .setDescription(```Welcome to the Endless! We're happy to have you on board. All of our D2 (and D1, occasionally) scheduling is done in the chats here. Join other people's groups or post up your own and people can join you.
-        if you have any questions just let any of us know and we got you```)
-        .setURL('https://www.bungie.net/en/ClanV2/Chat?groupId=1572587')
-        .setThumbnail('https://commons.wikimedia.org/wiki/Category:Destiny_2#/media/File:Destiny_2_logo.png')
-        .addField('Founder', 'JohnnyWebb', false)
-        .addField('Admins', 'RDSIII, KoreChan93, Fawkes35, DeadGirlParadise, Saucy_Daniell', true)
-        .setFooter('This message need updating? Ping @Kurayami-chanTheOriginalSpam');
-        message.channel.send({ embed: destinyEmbed });
+        message.channel.send({ embed: {
+            color: 0x0099ff,
+            title: 'The Endless',
+            url: 'https://www.bungie.net/en/ClanV2/Chat?groupId=1572587',
+            description: "Welcome to the Endless! We're happy to have you aboard. All of our D2 (and D1, occasionally) scheduling is done in the chats here. Join other people's groups or post up your own and people can join you. If you have any questions just let any of us know. We got you.",
+            image: {
+                url: 'https://commons.wikimedia.org/wiki/Category:Destiny_2#/media/File:Destiny_2_logo.png',
+            },
+            fields: [
+                {
+                    name: 'Founder',
+                    value: 'JohnnyWebb',
+                },
+                {
+                    name: 'Admins',
+                    value: 'RDSIII, KoreChan93, Fawkes35, DeadGirlParadise, Saucy_Daniell',
+                    inline: true,
+                },
+                {
+                    name: 'Clan URL',
+                    value: 'https://www.bungie.net/en/ClanV2/Chat?groupId=1572587',
+                },
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: 'This message need updating? Ping @Kurayami-chanTheOriginalSpam',
+            },
+        },
+      });
     }
 });
 
@@ -56,4 +70,4 @@ client.on('message', message => {
     }
 });
 
-client.login(config.token);
+client.login(token);
