fd7a9bae22aaebc96d1b24cf09e2c22f07d72bfa
xDp64xG <brow.paul64@gmail.com>
2018-05-10 19:58:32 -0400

    Update main.js
    Added in adding roles to elderscrollers and destiny (endless) on command. (Needs perms). Edited first message on member join to add in extra commands, to use command based on game.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find('name', channel-greeting);
     if (!channel) return;
-    channel.send(`Welcome to the Endless, ${member}! What's your PSN? What game are you here for?`);
+    channel.send(`Welcome to the Endless, ${member}! What's your PSN? What game are you here for? Use ``${prefix}(game) `` with game being the game you play! A list of games are: Destiny~~, Monster-Hunter, ~~and ESO.`);
 });
 
 client.on('message', message => {
@@ -36,7 +36,7 @@ client.on('message', message => {
             color: 0x0099ff,
             title: 'The Endless',
             url: 'https://www.bungie.net/en/ClanV2/Chat?groupId=1572587',
-            description: "Welcome to the Endless! We're happy to have you aboard. All of our D2 (and D1, occasionally) scheduling is done in the chats here. Join other people's groups or post up your own and people can join you. If you have any questions just let any of us know. We got you.",
+            description: "Welcome to the Endless! We're happy to have you aboard. All of our D2 (and D1, occasionally) scheduling is done in the chats here. Join other people's groups or post up your own and people can join you. If you have any questions just let any of us know. We got you. If you would like to join, use ``${prefix}destiny-join`` to join.",
             image: {
                 url: 'https://commons.wikimedia.org/wiki/Category:Destiny_2#/media/File:Destiny_2_logo.png',
             },
@@ -61,6 +61,24 @@ client.on('message', message => {
             },
         },
       });
+    }
+});
+
+client.on('message', message => {
+    if (message.content ===`${prefix}destiny-join`) {
+        let role = message.guild.roles.find("name", "endless");
+        let member = message.member;
+        member.addRole(role).catch(console.error);
+        channel.send("May or may not have added the role.")
+    }
+});
+
+client.on('message', message => {
+    if (message.content ===`${prefix}eso-join`) {
+        let role = message.guild.roles.find("name", "elderscrollers");
+        let member = message.member;
+        member.addRole(role).catch(console.error);
+        channel.send("May or may not have added the role.")
     }
 });
 
