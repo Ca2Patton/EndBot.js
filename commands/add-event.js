9efe247036b8a89507393ae18ddba7b154a37828
Caitlin Patton <kittyluva2@gmail.com>
2018-05-11 15:45:57 -0700

    Now with commentary

--- commands/add-event.js
+++ commands/add-event.js
@@ -6,10 +6,13 @@ module.exports = {
     description: 'Add scheduled event to the calendar.',
     args: true,
     execute(message, args) {
+        // Insert Collector of some sort here.
+
+        // Gotta fix this up after collecting the appropriate data.
         const searchClient = new SwiftypeAppSearchClient(st_host_key, st_api_key);
         document = [
             {
-                id: "", // Must be unique for each document.
+                id: "", // Must be unique for each document. Maybe use the message id?
                 title: "",
                 description: "",
                 event_date: "", // Must be an ISO 8601 Format Date.
