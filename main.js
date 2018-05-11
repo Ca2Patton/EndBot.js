85712031c16316feb78ff81f66fa37872aa301a5
Caitlin Patton <kittyluva2@gmail.com>
2018-05-11 08:31:42 -0700

    Does this fix the issue?
    We shall see.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { Client, MessageEmbed } = require('discord.js');
-const { prefix, token, channel_greeting } = require('./config.json');
 const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-const client = new Client();
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
+
 const commandFiles = fs.readdirSync('./commands');
 
 for (const file of commandFiles) {
@@ -11,33 +12,80 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
-if (!message.content.startsWith(prefix) || message.author.bot) return;
-
 // Ensure that endbot is running
 client.on('ready', () => {
     console.log('Endbot ready for chat!');
 });
 
 
-const args = message.content.slice(prefix.length).split(/ +/);
-const commandName = args.shift().toLowerCase();
-
-if (!client.commands.has(commandName)) return;
-
-const command = client.commands.get(commandName);
-
-if (command.args && !args.length) {
-    return message.channel.send(`You didn't provide any arguments, ${message.author}!`)
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
-try {
-    command.execute(message, args);
-}
+const cooldowns = new Discord.Collection();
 
-catch(error) {
-    console.error(error);
-    message.reply('There was an error trying to execute the command! Please notify @Kurayami-chanTheOriginalSpam if the issue persists.');
-}
+client.on('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (!timestamps.has(message.author.id)) {
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+	else {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
 
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find('name', channel_greeting);
