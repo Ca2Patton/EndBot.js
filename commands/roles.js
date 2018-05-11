f43b28a9f200c64cd8085821aa541304b7e646e3
Caitlin Patton <kittyluva2@gmail.com>
2018-05-11 13:08:57 -0700

    Trying again

--- commands/roles.js
+++ commands/roles.js
@@ -24,10 +24,8 @@ module.exports = {
                     errors: ['time']
                 })
                 .then(collected => {
-                    const role = message.guild.role.find('name', `${collected.first().content}`);
+                    const role = collected.first().content;
                     message.channel.send(`${member} specified role is ${role}.`);
-
-
                 })
                 .catch(collected => {
                     message.channel.send('Timed out waiting for a response. Please try again.');
