8ca1f1fe7ee5a1086077baf1ce46c5b9c563c600
Caitlin Patton <kittyluva2@gmail.com>
2018-05-11 12:26:12 -0700

    Added a role command

--- commands/destiny.js
+++ commands/destiny.js
@@ -7,7 +7,7 @@ module.exports = {
                 color: 0x0099ff,
                 title: 'The Endless',
                 url: 'https://www.bungie.net/en/ClanV2/Chat?groupId=1572587',
-                description: "Welcome to the Endless! We're happy to have you aboard. All of our D2 (and D1, occasionally) scheduling is done in the chats here. Join other people's groups or post up your own and people can join you. If you have any questions just let any of us know. We got you.",
+                description: "Welcome to the Endless! We're happy to have you aboard. All of our D2 (and D1, occasionally) scheduling is done in the chats here.\nJoin other people's groups or post up your own and people can join you. If you have any questions just let any of us know. We got you.",
                 image: {
                     url: 'https://commons.wikimedia.org/wiki/Category:Destiny_2#/media/File:Destiny_2_logo.png',
                 },
---
+++ commands/roles.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'add-role',
+    description: 'Add a user to one of the chat groups',
+    args: true,
+    execute(message, args) {
+        if (!message.guild) return;
+
+        const member = message.mentions.members.first();
+        // Prompt user for a response.
+
+        const roles = [{
+            "question": "Which role is the user being added to?",
+            "answers": ["bosses", "endless", "overwatchheros", "elderscrollers", "esocrafters", "trekkies", "minecrafters", "fortniters", "darksoulers"]
+        }];
+        const item = roles[Math.floor(Math.random() * roles.length)];
+        const filter = response => {
+            return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+        };
+
+        message.channel.send(item.question).then(() => {
+            message.channel.awaitMessages(filter, {
+                    maxMatches: 1,
+                    time: 3000,
+                    errors: ['time']
+                })
+                .then(collected => {
+                    const role = message.guild.role.find('name', `${collected.first}`);
+                    message.channel.send(`Specified role is ${role}.`);
+                })
+                .catch(collected => {
+                    message.channel.send('Timed out waiting for a response. Please try again.');
+                });
+        });
+    }
+};
