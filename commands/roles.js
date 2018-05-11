b4a418a9bb66e4a2564902f5467b8ef9002bfd01
Caitlin Patton <kittyluva2@gmail.com>
2018-05-11 12:48:19 -0700

    Lets try this again, shall we?

--- commands/roles.js
+++ commands/roles.js
@@ -24,7 +24,8 @@ module.exports = {
                     errors: ['time']
                 })
                 .then(collected => {
-                    message.channel.send(`Specified role is ${collected.first()}.`);
+                    const role = message.guild.role.find('name', `${collected.first()}`);
+                    message.channel.send(`Specified role is ${role}.`);
                 })
                 .catch(collected => {
                     message.channel.send('Timed out waiting for a response. Please try again.');
